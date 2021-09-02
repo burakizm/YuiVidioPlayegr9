e5878d2ca087360efb14000eddf845367fd128a3
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-02 18:32:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+from os import getenv
 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
+API_ID = int(os.getenv("API_ID", "2107384"))
+API_HASH = os.getenv("API_HASH", "43ed88f662a3fcdaf787fef864fb37fa")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "1929345356:AAHC2MMfuZrhI1e4RY9vwHx6V0T6CfswHoA")
+SESSION_NAME = os.getenv("SESSION_NAME", "BQAX_D4IqcbOe9oYWBIOQ2qBYj2mo65dRZRRtuSx5Hb3CsLH1OB9ZrahqbBH1afGHf-UGxfKVAotv2ocdBWxyPRwn-wryTfW4zTGzeWle0Vl0Y_q6ZWtR0ZARhFTCQNaxXtxp8koXhueUDwCVYbwr0Ossixu3NTumnYPu6FwJkmNPHNUdgo9sL-V_WK4U-iUvqw6IOQEH1eibCZSn76u1BeXybeCpvw_3-4SaRL6M7ftUb9-LSUuyIzaAfu4vX0Wr6IpQ1YthaWXefwS3vTfawJmuCWsqrebT8TChrnu9KAXfI0C2jZ9NyBN6DZtHufIy9hLgcVHLalESEAcQCv78fpJUtHJBAA")
