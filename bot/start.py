1238d05dd54900d428c3d7a6932d869862f0ca1e
murtixc <114428056+murtixc@users.noreply.github.com>
2022-10-07 01:10:57 +0300

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**Merhaba!Ben telegram gruplarında görüntülü sohbette video yayını yapabilen bir botum. \n\n**Type /help komutlar için:-** __ \n1) Yazınız /info kurucu için`",   
+       await m.reply(f"**Merhaba!Ben telegram gruplarında görüntülü sohbette video yayını yapabilen bir botu. \n\n**Type /help komutlar için:-** __ \n1) Yazınız /info kurucu için`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
